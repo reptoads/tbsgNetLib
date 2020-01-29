5e69681345b88152afa532d8a3bf6b025445303b
Simon Renger <simon.renger@gmail.com>
2020-01-29 16:44:36 +0100

    init commit

---
+++ include/tbsg/tbsgNetLib.hpp
@@ -0,0 +1,13 @@
+#ifndef TBSG_TBSGNETLIB_HPP_INCLUDED
+#define TBSG_TBSGNETLIB_HPP_INCLUDED
+
+namespace tbsg {
+
+/**
+ * Calculate the answer. Not sure what the question is, though...
+ */
+int calculate_value();
+
+}  // namespace tbsg
+
+#endif // TBSG_TBSGNETLIB_HPP_INCLUDED
